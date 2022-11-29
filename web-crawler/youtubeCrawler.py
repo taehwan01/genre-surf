9152b10b69d6d4efd3d81de4ffa1a874a30bd55e
Monte <monte6198@gmail.com>
2022-11-29 17:50:42 +0900

    feat: web-crawler init

---
+++ web-crawler/youtubeCrawler.py
@@ -0,0 +1,11 @@
+# 이 폴더 clone 해서 받았으면,
+# git checkout -b [너 git 아이디]
+# 위 명령어로 너의 브랜치를 하나 파고 이 브랜치 안에서 네 코드들 작성해
+# 이 폴더 안에서 소스파일들 너가 원하는대로 생성해서 파일 작성해
+# 파일은 최대한 한 기능마다 또는 한 함수마다 commit 남기고
+# commit 방식은 아래 예시처럼
+# feat(youtube-music-crawler): 웹 호출하여 데이터 추출
+# feat(youtube-music-crawler): 웹에서 가져온 데이터 가공
+# commit 메시지는 인터넷에서 "git commit 메시지" 치면 다 나오는데, feat, style, docs, refactor 등등 다양해
+# 커밋 메시지 중에서 너가 알맞는 거 고르고, 너는 유튜브 뮤직 웹 크롤링 담당하기로 했으니까 feat(youtube-music-crawler) 이런 식으로 괄호에 담당하는 부분들 남겨
+print("원식이 파트");
