06edef64a3e85c1b3810925fe02d1a917812393a
pyowonsik <qqrtyu@gmail.com>
2022-12-05 17:21:57 +0900

    refactor youtube_music_crawler code

--- web-crawler/youtubeCrawler.py
+++ web-crawler/youtubeCrawler.py
@@ -14,12 +14,16 @@ from bs4 import BeautifulSoup
 import urllib.request
 import pandas as pd
 import datetime
+import random
 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service #
 from webdriver_manager.chrome import ChromeDriverManager #
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 
@@ -27,12 +31,51 @@ def YouTube_music(result):
     wd = webdriver.Chrome(service =Service(ChromeDriverManager().install()))
 
 
-    search_name = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae"]
+    #search_name = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae"]
     #YouTube_URL = (f"https://www.youtube.com/results?search_query={search_name}")
+    keyword = 'genre song'
 
-    for i in range(0,9):
-        wd.get(f"https://www.youtube.com/results?search_query={search_name[i]}")
-        time.sleep(1)
+    wd.get('https://youtube.com/');
+    time.sleep(3)
+
+    search = wd.find_element(By.NAME, "search_query")
+    time.sleep(2)
+    search.send_keys(keyword)
+    search.send_keys(Keys.ENTER)
+    print(keyword + "검색")
+    
+    html = wd.page_source
+    soupYM = BeautifulSoup(html,'html.parser')
+
+
+    try:        
+        # 페이지 내 스크롤 높이 받아오기
+        last_page_height = wd.execute_script("return document.documentElement.scrollHeight")
+        while True:
+            # 임의의 페이지 로딩 시간 설정
+            # PC환경에 따라 로딩시간 최적화를 통해 scraping 시간 단축 가능
+            pause_time = random.uniform(1, 2)
+            # 페이지 최하단까지 스크롤
+            wd.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+            # 페이지 로딩 대기
+            time.sleep(pause_time)
+            # 무한 스크롤 동작을 위해 살짝 위로 스크롤(i.e., 페이지를 위로 올렸다가 내리는 제스쳐)
+            wd.execute_script("window.scrollTo(0, document.documentElement.scrollHeight-50)")
+            time.sleep(pause_time)
+            # 페이지 내 스크롤 높이 새롭게 받아오기
+            new_page_height = wd.execute_script("return document.documentElement.scrollHeight")
+            # 스크롤을 완료한 경우(더이상 페이지 높이 변화가 없는 경우 && result.length == 50)
+            if new_page_height == last_page_height:
+                print("스크롤 완료")
+                print(result)
+                break
+            # 스크롤 완료하지 않은 경우, 최하단까지 스크롤
+            else:
+                last_page_height = new_page_height
+    except Exception as e:
+        print("에러 발생: ", e)
+                
+
     
 
     return
