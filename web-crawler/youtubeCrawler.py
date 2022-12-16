c92fb5dc3d7225a8751f52a2a5e5916dd4ee7c00
pyowonsik <qqrtyu@gmail.com>
2022-12-16 17:09:07 +0900

    feat : crawling method

--- web-crawler/youtubeCrawler.py
+++ web-crawler/youtubeCrawler.py
@@ -27,117 +27,99 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 wd = webdriver.Chrome(service =Service(ChromeDriverManager().install()))
-
-
-def scroll():    
-    try:        
-        # 페이지 내 스크롤 높이 받아오기
-        last_page_height = wd.execute_script("return document.documentElement.scrollHeight")
-        while True:
-            # 임의의 페이지 로딩 시간 설정
-            # PC환경에 따라 로딩시간 최적화를 통해 scraping 시간 단축 가능
-            pause_time = random.uniform(1, 2)
-            # 페이지 최하단까지 스크롤
-            wd.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-            # 페이지 로딩 대기
-            time.sleep(pause_time)
-            # 무한 스크롤 동작을 위해 살짝 위로 스크롤(i.e., 페이지를 위로 올렸다가 내리는 제스쳐)
-            wd.execute_script("window.scrollTo(0, document.documentElement.scrollHeight-50)")
-            time.sleep(pause_time)
-            # 페이지 내 스크롤 높이 새롭게 받아오기
-            new_page_height = wd.execute_script("return document.documentElement.scrollHeight")
-            # 스크롤을 완료한 경우(더이상 페이지 높이 변화가 없는 경우 && result.length == 50)
-            if new_page_height == last_page_height:
-                print("스크롤 완료")
-                break
-            # 스크롤 완료하지 않은 경우, 최하단까지 스크롤
-            else:
-                last_page_height = new_page_height
-    except Exception as e:
-        print("에러 발생: ", e)
     
     
+def YouTube_music():
 
-def YouTube_music(result):
-    
-    keyword ='blues'
+    keyword ='pop genre song'
     wd.get('https://youtube.com/');
-    time.sleep(3)
-    
-    
 
-    # keyword 입력
+
     search = wd.find_element(By.NAME, "search_query")
     time.sleep(2)
-    search.send_keys(keyword) 
-    #search.send_keys(Keys.ENTER)
-    time.sleep(2)
-    # search 버튼 클릭
+    search.send_keys(keyword)
+
     btnSearch = wd.find_element(By.ID, 'search-icon-legacy')
     btnSearch.click()
     print(keyword + "검색")
     time.sleep(3)
 
-    # 검색 필터를 4k로 설정해서 shorts , 실시간 막을것
-    #sfillter = wd.find_element(By.XPATH,'//*[@id="container"]/ytd-toggle-button-renderer/a')
-    #sfillter.click()
-    scroll()
-      
+
+    fillter = wd.find_element(By.XPATH,'//*[@id="container"]/ytd-toggle-button-renderer')
+    fillter.click()
+    time.sleep(5)
+
+    timefillter = wd.find_element(By.XPATH, '//html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[2]/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[3]/ytd-search-filter-renderer[3]/a')
+    timefillter.click()
+    time.sleep(5)
+
+
     html = wd.page_source
     soupYM = BeautifulSoup(html,'html.parser')
-    content_total = soupYM.find_all(class_ = 'yt-simple-endpoint style-scope ytd-video-renderer')
-    content_total_title = list(map(lambda data: data.get_text().replace("\n", ""), content_total))
-    content_total_link = list(map(lambda data: "https://youtube.com" + data["href"], content_total))
-    content_play_time = list(map(lambda data: data['aria-label'],content_total))
+    body = wd.find_element(By.CSS_SELECTOR,'body')
 
 
-    add_btn = soupYM.find_all(class_ = 'dropdown-trigger style-scope ytd-menu-renderer')
+    add_btn = []
 
-    new_content_total_time = []
-    new_content_total_title = []
-    new_content_total_link = []
+    content_total = soupYM.find_all(class_ = 'style-scope ytd-thumbnail-overlay-time-status-renderer')
+    content_total = list(map(lambda data: data.get_text().replace("\n", ""), content_total))
 
-    #for i in range(0,(len(content_play_time)-1)):
-        #content_play_time[i] = content_play_time[i].split('전')
-        #content_play_time[i] = content_play_time[i][1]
-        #content_play_time[i] = content_play_time[i].split('조회수')
-        #content_play_time[i] = content_play_time[i][0]
-        #content_play_time[i] = content_play_time[i][0].strip()
-        #print(content_play_time[i])
-    #print(content_total_title)
+    #content_play_time = list(map(lambda data: data.get_text().replace("\n", ""), content_total))
+    #content_play_time = content_play_time[1::2]
+    #content_play_time[i] = content_play_time[i].strip()
+    #print(content_play_time)
+            
+
+    for i in range(0,(len(content_total)-1)):
+        add_btn.append(wd.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[2]/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer['+str(i+1)+']/div[1]/div/div[1]/div/div/ytd-menu-renderer/yt-icon-button/button'))
+        add_btn[i].click()
+        time.sleep(3)
+        add_btn_select = wd.find_element(By.XPATH, '/html/body/ytd-app/ytd-popup-container/tp-yt-iron-dropdown/div/ytd-menu-popup-renderer/tp-yt-paper-listbox/ytd-menu-service-item-renderer[1]')
+        add_btn_select.click()
+        wd.execute_script("window.scrollTo(" + str(i*220)+ "," +  str((i+1)*220) + ");")
+        time.sleep(5)
+
+        # total_time == 24시간
+        if(i == 4) :
+            body.send_keys('k')
+            break
+
+    print("크롤링 종료")
 
 
-    
-    for i in range(0,(len(content_play_time)-1)):
-        print(content_total_title[i])
-        print(content_play_time[i])
-        content_play_time[i] = content_play_time[i].split('전')
-        content_play_time[i] = content_play_time[i][1]
-        content_play_time[i] = content_play_time[i].split('분')
-        content_play_time[i] = content_play_time[i][0]
-        content_play_time[i] = re.sub(r'[^0-9]', '', content_play_time[i])
-        print(content_play_time[i])
-        if((int(content_play_time[i])>1) and (int(content_play_time[i])<7)):
-            new_content_total_time.append(int(content_play_time[i]))
-            new_content_total_title.append(content_total_title[i])
-            new_content_total_link.append(content_total_link[i])
+            
+#    content_total = soupYM.find_all(class_ = 'yt-simple-endpoint style-scope ytd-video-renderer')
+#    content_total_title = list(map(lambda data: data.get_text().replace("\n", ""), content_total))
+#    content_total_link = list(map(lambda data: "https://youtube.com" + data["href"], content_total))
+#    content_play_time = list(map(lambda data: data['aria-label'],content_total))
+#    new_content_total_time = []
+#   new_content_total_title = []
+#    new_content_total_link = []
+#    for i in range(0,(len(content_play_time)-1)):
+#        print(content_total_title[i])
+#        print(content_play_time[i])
+#        content_play_time[i] = content_play_time[i].split('전')
+#        content_play_time[i] = content_play_time[i][1]
+#       content_play_time[i] = content_play_time[i].split('분')
+#        content_play_time[i] = content_play_time[i][0]
+#        content_play_time[i] = re.sub(r'[^0-9]', '', content_play_time[i])
+#        print(content_play_time[i])
+#        if((int(content_play_time[i])>1) and (int(content_play_time[i])<7)):
+#           new_content_total_time.append(int(content_play_time[i]))
+#            new_content_total_title.append(content_total_title[i])
+#            new_content_total_link.append(content_total_link[i])
             #if(len(new_content_total_title) == 51):
                 #continue
-            result.append([content_total_title[i]] + [content_total_link[i]])
-        print(new_content_total_time,new_content_total_title,new_content_total_link)
+#            result.append([content_total_title[i]] + [content_total_link[i]])
+#        print(new_content_total_time,new_content_total_title,new_content_total_link)
        
 
 
     
-
-
 def main():
     result = []
     print("YouTubeMusic crawling >>>>>>>>>>>>>>>>>>>>")
-    YouTube_music(result)
-
-    CB_tbl = pd.DataFrame(result,columns=('title','link'))
-    CB_tbl.to_csv('./YoutubeMusic.csv',encoding='utf-8',mode='w',index=True)
+    YouTube_music()
 
 if __name__ == '__main__':
     main()
