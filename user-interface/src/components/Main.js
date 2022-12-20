30729c4d2cd1a7e22166ac7b3bb5f579d8011514
Monte <monte6198@gmail.com>
2022-12-21 07:06:59 +0900

    feat: 파일 업로드, 장르 추출 api 호출 결합

--- user-interface/src/components/Main.js
+++ user-interface/src/components/Main.js
@@ -1,18 +1,38 @@
 import { useState } from 'react';
 import axios from 'axios';
 import '../css/Main.css';
+// import { upload } from '@testing-library/user-event/dist/upload';
 
 function Main() {
-  let [genre, setGenre] = useState('\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0');
+  let [genre, setGenre] = useState(
+    '\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0',
+  );
 
-  const getGenre = () => {
-    console.log('Execute getGenre');
-    axios.get('http://localhost:8080/get-genre')
-      .then((response) => {
-        setGenre(response.data)
-        console.log(response)
+  const apiClient = axios.create({
+    baseURL: 'http://localhost:8080',
+  });
+
+  const uploadFile = async (file) => {
+    console.log('upload file and get genre.');
+    try {
+      const { data } = await apiClient.post('/get-genre', file).then((response) => {
+        setGenre(response.data);
+        console.log(response);
       });
-  }
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const upload = async (event) => {
+    console.log('file: ', event.target.file);
+    event.preventDefault();
+    console.log('files[0]: ', event.target.file.files[0]);
+    const formData = new FormData();
+    formData.append('file', event.target.file.files[0]);
+    await uploadFile(formData);
+  };
 
   const youtubeButton = () => {
     window.open('https://www.youtube.com/', '_blank');
@@ -22,12 +42,12 @@ function Main() {
     <div className='main'>
       <div className='box-content'>
         <h1>Genre Surf</h1>
-        <label className='file-button' htmlFor='input-file'>
-          Upload Audio File
-        </label>
-        <input type='file' id='input-file' />
-        <br />
-        <button className='search-button' onClick={getGenre}>What is the genre?</button>
+        <form encType='multipart/form-data' onSubmit={upload}>
+          <input type='file' name='file' id='input-file' />
+          <button type='submit' className='file-button'>
+            Get genre !
+          </button>
+        </form>
         <h3>
           Your audio file's genre is:&nbsp;
           <u>&nbsp;&nbsp;</u>
