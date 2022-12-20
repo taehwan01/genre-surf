7ea266b0351ccd62c18fa95d8b286b8625907fa1
pyowonsik <qqrtyu@gmail.com>
2022-12-20 23:58:10 +0900

    crawling time range and complete

--- web-crawler/youtubeCrawler.py
+++ web-crawler/youtubeCrawler.py
@@ -24,6 +24,7 @@ from selenium.webdriver.chrome.service import Service #
 from webdriver_manager.chrome import ChromeDriverManager #
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 
 
 wd = webdriver.Chrome(service =Service(ChromeDriverManager().install()))
@@ -47,11 +48,20 @@ def YouTube_music():
 
     fillter = wd.find_element(By.XPATH,'//*[@id="container"]/ytd-toggle-button-renderer')
     fillter.click()
-    time.sleep(5)
+    time.sleep(3)
 
-    timefillter = wd.find_element(By.XPATH, '//html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[2]/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[3]/ytd-search-filter-renderer[3]/a')
-    timefillter.click()
-    time.sleep(5)
+    timefillter_1 = wd.find_element(By.XPATH, '//html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[2]/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[3]/ytd-search-filter-renderer[3]/a')
+    timefillter_1.click()
+    time.sleep(3)
+
+    fillter = wd.find_element(By.XPATH,'//*[@id="container"]/ytd-toggle-button-renderer')
+    fillter.click()
+    time.sleep(3)
+
+
+    timefillter_2 = wd.find_element(By.XPATH, '//html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[2]/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[4]/ytd-search-filter-renderer[2]/a')
+    timefillter_2.click()
+    time.sleep(3)
 
 
     html = wd.page_source
@@ -61,30 +71,68 @@ def YouTube_music():
 
     add_btn = []
 
-    content_total = soupYM.find_all(class_ = 'style-scope ytd-thumbnail-overlay-time-status-renderer')
-    content_total = list(map(lambda data: data.get_text().replace("\n", ""), content_total))
+    # 전체 컨텐츠 
+    #content_total = soupYM.find_all(class_ = 'style-scope ytd-thumbnail-overlay-time-status-renderer')
+    #content_total_time = soupYM.find_all(class_ = 'ytp-time-duration').text
+    #content_total = list(map(lambda data: data.get_text().replace("\n", ""), content_total))
 
     #content_play_time = list(map(lambda data: data.get_text().replace("\n", ""), content_total))
     #content_play_time = content_play_time[1::2]
     #content_play_time[i] = content_play_time[i].strip()
     #print(content_play_time)
             
+    i = 0
+    total_hour = 0
+    total_minute = 0
+    while True:
 
-    for i in range(0,(len(content_total)-1)):
         add_btn.append(wd.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[2]/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer['+str(i+1)+']/div[1]/div/div[1]/div/div/ytd-menu-renderer/yt-icon-button/button'))
         add_btn[i].click()
         time.sleep(3)
+
+        content_time = wd.find_element(By.XPATH,'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div[2]/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[2]/div[3]/ytd-video-renderer['+str(i+1)+']/div[1]/ytd-thumbnail/a/div[1]/ytd-thumbnail-overlay-time-status-renderer/span').text
+
+        
         add_btn_select = wd.find_element(By.XPATH, '/html/body/ytd-app/ytd-popup-container/tp-yt-iron-dropdown/div/ytd-menu-popup-renderer/tp-yt-paper-listbox/ytd-menu-service-item-renderer[1]')
         add_btn_select.click()
+            
+      
+        if(len(content_time) < 6):
+            content_time = '00:'+str(content_time)
+            
+        print('영상시간 :' + content_time)    
+        content_time = datetime.strptime(content_time,"%H:%M:%S")
+        total_hour = total_hour + content_time.hour
+        total_minute = total_minute + content_time.minute
+        print(total_hour)
+        print(total_minute)
+
+        if(total_minute > 60 or total_minute == 60):
+            total_minute = total_minute - 60
+            total_hour = total_hour + 1
+        
+        time.sleep(3)
         wd.execute_script("window.scrollTo(" + str(i*220)+ "," +  str((i+1)*220) + ");")
+
+        
+        
+        i = i + 1
         time.sleep(5)
 
-        # total_time == 24시간
-        if(i == 4) :
+        if(total_hour > 23) :
             body.send_keys('k')
             break
+        
 
     print("크롤링 종료")
+
+        #unfold_btn = wd.find_element(By.XPATH, '/html/body/ytd-app/ytd-miniplayer/div[2]/div/div[1]/div[2]/div[2]/yt-icon-button/button')
+        #unfold_btn.click()
+        #time.sleep(3)
+
+        #fold_btn = wd.find_element(By.XPATH, '/html/body/ytd-app/ytd-miniplayer/div[2]/div/div[1]/div[2]/div[2]/yt-icon-button/button')
+        #fold_btn.click()
+        #time.sleep(3)
 
 
             
@@ -115,6 +163,15 @@ def YouTube_music():
        
 
 
+    
+        #unfold_btn = wd.find_element(By.XPATH, '/html/body/ytd-app/ytd-miniplayer/div[2]/div/div[1]/div[2]/div[2]/yt-icon-button/button')
+        #unfold_btn.click()
+        #time.sleep(3)
+        #fold_btn = wd.find_element(By.XPATH, '/html/body/ytd-app/ytd-miniplayer/div[2]/div/div[1]/div[2]/div[2]/yt-icon-button/button')
+        #fold_btn.click()
+        #time.sleep(3)
+
+        
     
 def main():
     result = []
